d45cd5f2e38bb8a40e004c6aa7347bfb312c95c1
Rodrigo <rodrigo.dealmeidadias@gmail.com>
2023-11-02 23:42:01 -0300

    Commit de treinamento Angular 14

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(
+      private service: PensamentoService
+      )
+      {
+
+      }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+
+
+}
